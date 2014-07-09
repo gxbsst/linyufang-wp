379a56b02e81fcbf07c3482f0479a6bf33735e4a
gxbsst <gxbsst@gmail.com>
2014-07-09 13:38:42 +0800

    支持隐藏浏览器的地址栏和工具栏

--- public/content/themes/mangguozhixiang/header.php
+++ public/content/themes/mangguozhixiang/header.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
-    <title></title>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width, minimal-ui">
+    <title>芒果之乡</title>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
     <?php wp_head(); ?>
 </head>
   <body ng-app="starter" animation="slide-left-right-ios7">
-
